886854979148ce5edfe6879d1a6944e0c9d1617e
Julia Wey-Burgoyne <juliawey@gmail.com>
2023-07-13 20:08:38 -0400

    JavaScript Calculator Challenge Lab Complete

---
+++ index.js
@@ -0,0 +1,29 @@
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b
+}
+function multiply(a, b) {
+    return a * b
+}
+function divide(a, b) {
+    return a / b
+}
+let n = 5;
+function increment(a){
+    n = a + 1;
+    return n;
+}
+function decrement(a){
+    n = a - 1;
+    return n;
+}
+function makeInt(a){
+    n = parseInt(a, 10);
+    return n;
+}
+function preserveDecimal(a){
+    n = parseFloat(a, 10);
+    return n;
+}
